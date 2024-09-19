594b593cd3a05decf5589989ce37a376cf6d4fa9
Pannajade <pannajade@hotmail.com>
2024-09-19 15:58:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const submitButton = document.getElementById('submitButton')
+submitButton.addEventListener("click", addTodo)
+
+function addTodo() {
+    const valueTodo = document.getElementById('todo').value
+    // const p = document.createElement('p')
+    // p.innerHTML = valueTodo
+    const result = document.getElementById('result')
+    // result.appendChild(p)
+    result.innerHTML += `<li>${valueTodo}</li>`
+}
+
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//     .then(response => response.json())
+//     .then(json => {
+//         console.log(json)
+//         for (let i = 0; i < json.length; i++) {
+//             const result = document.getElementById("result");
+//             result.innerHTML += `<li>${json[i].title}</li>`;
+//         }
+//     })
+
+async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const json = await response.json()
+
+    for (let i = 0; i < json.length; i++) {
+        const result = document.getElementById("result");
+        result.innerHTML += `<li>${json[i].title}</li>`;
+    }
+}
+
+getData()
